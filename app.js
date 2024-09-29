01b9f506548cc78dfbaae1c4678f555d4130a70a
pablodev <pabloteixeira591@gmail.com>
2024-09-29 06:31:51 -0300

    Add files via upload

--- app.js
+++ app.js
@@ -1,3 +1,11 @@
 document.addEventListener("DOMContentLoaded", () => {
     alert('Esse portfólio ainda está em desenvolvimento, então é provável que algumas coisas não funcionem')
 })
+
+const menuBtn = document.getElementById('menu')
+const menu = document.getElementById('menu-container')
+
+menuBtn.addEventListener('click', () => {
+    menu.classList.toggle('hidden')
+})
+
